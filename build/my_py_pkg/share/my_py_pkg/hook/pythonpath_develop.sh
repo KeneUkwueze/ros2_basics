3f0d25541cac00b85c956d09022a01359ab7f119
kene ukwueze <kayceedivine@gmail.com>
2022-01-12 05:40:32 +0100

    implemented and created topic and msg for the python number_publisher node

--- build/my_py_pkg/share/my_py_pkg/hook/pythonpath_develop.sh
+++ build/my_py_pkg/share/my_py_pkg/hook/pythonpath_develop.sh
@@ -1,3 +1,3 @@
 # generated from colcon_core/shell/template/hook_prepend_value.sh.em
 
-_colcon_prepend_unique_value PYTHONPATH "/home/kenechi/ros2_ws/build/my_py_pkg"
+_colcon_prepend_unique_value PYTHONPATH "/home/kenechi/ros2_ws/ros2_basics/build/my_py_pkg"
