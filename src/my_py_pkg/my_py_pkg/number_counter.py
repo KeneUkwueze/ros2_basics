bfcdbdcc9590d99a8ba8321c28a94d7e6d4fe636
kene ukwueze <kayceedivine@gmail.com>
2022-01-14 05:43:26 +0100

    implemented number_counter node to subcribe to the number topic and output the messages to the terminal

--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from example_interfaces.msg import Int64
 
 
-class NumberCounterNode(Node): 
+class NumberCounterNode(Node):
     def __init__(self):
-        super().__init__("number_counter") 
+        super().__init__("number_counter")
+        self.subscriber_ = self.create_subscription(
+            Int64, "numbers", self.callback_number_counter, 10)
+        self.get_logger().info("Number counter has started...")
+
+    def callback_number_counter(self, msg):
+        self.get_logger().info(str(msg.data))
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = NumberCounterNode() 
+    node = NumberCounterNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
